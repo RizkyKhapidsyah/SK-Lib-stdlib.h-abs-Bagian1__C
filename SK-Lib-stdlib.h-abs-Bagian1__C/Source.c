d242dcb127c6c95a490400793b23f0fd67e99bae
Rizky Khapidsyah <rizkykhapidsyah@gmail.com>
2023-09-20 10:58:41 +0700

    Initial commit

---
+++ SK-Lib-stdlib.h-abs-Bagian1__C/Source.c
@@ -0,0 +1,22 @@
+#include <stdio.h>      
+#include <stdlib.h>     
+#include <conio.h>
+
+/*
+	Source by CPlusPlus (https://www.cplusplus.com)
+	Modified For Learn by RK
+	I.D.E : VS2022
+*/
+
+int main() {
+	int n, m;
+
+	n = abs(23);
+	m = abs(-11);
+	
+	printf("Nilai n = %d\n", n);
+	printf("Nilai m = %d\n", m);
+	
+	_getch();
+	return 0;
+}
